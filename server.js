6230b52200ee0a7ccc4c2fb2ef51066b24385995
tarunkumar092004 <tarun@example.com>
2026-05-19 23:44:30 +0530

    Add root route to serve index.html

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use('/auth', authRoutes);
+app.use(express.static('.')); app.get('/', (req, res) => {     res.sendFile(__dirname + '/index.html'); });
 
 const dbURI = 'mongodb+srv://tarunkumar0920004:Sontra143404040@neuraxcluster.xyfaw58.mongodb.net/?appName=NeuraXCluster';
 
